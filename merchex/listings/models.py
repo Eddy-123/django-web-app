44a02637603349d5520d6190a9cfc4492e05bc15
Eddy-123 <adegnandjoueddy12@gmail.com>
2022-03-21 11:52:20 +0100

    P3-C1: read data in a view

--- merchex/listings/models.py
+++ merchex/listings/models.py
@@ -1,8 +1,45 @@
+from pyexpat import model
 from unicodedata import name
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 class Band(models.Model):
+    class Genre(models.TextChoices):
+        HIP_HOP = 'HH'
+        SYNTH_POP = 'SP'
+        ALTERNATIVE_ROCK = 'AR'
+
+
     name = models.fields.CharField(max_length=100)
+    genre = models.fields.CharField(choices=Genre.choices, max_length=5)
+    biography = models.fields.CharField(max_length=1000)
+    year_formed = models.fields.IntegerField(
+        validators=[MinValueValidator(1900), MaxValueValidator(2022)]
+    )
+    active = models.fields.BooleanField(default=True)
+    official_homepage = models.fields.URLField(null=True, blank=True)
+    #like_new = models.fields.BooleanField(default=False)
+
+    def __str__(self):
+        return f'{self.name}'
 
 class Listing(models.Model):
+    class Type(models.TextChoices):
+        RECORDS = 'RE'
+        CLOTHING = 'CL'
+        POSTERS = 'PO'
+        MISCELLANEOUS = 'MI'
+
     title = models.fields.CharField(max_length=100)
+    description = models.fields.CharField(max_length=100)
+    sold = models.fields.BooleanField(default=False)
+    year = models.fields.IntegerField(
+        validators=[MinValueValidator(1200), MaxValueValidator(2022)],
+        null=True
+    )
+    type = models.fields.CharField(choices=Type.choices, max_length=5)
+    band = models.ForeignKey(Band, null=True, on_delete=models.SET_NULL)
+    #bidon = models.fields.BooleanField(null=True)
+
+    def __str__(self):
+        return f'{self.title}'
--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -3,36 +3,35 @@ from django.http import HttpResponse
 from listings.models import Band, Listing
 
 
-def hello(request):
+def band_list(request):
     bands = Band.objects.all()
-    return HttpResponse(f"""
-        <h1>hello Django!</h1>
-        <p>Mes groupes preferes sont:</p>
-        <ul>
-            <li>{bands[0].name}</li>
-            <li>{bands[1].name}</li>
-            <li>{bands[2].name}</li>
-        </ul>
-    """)
+    return render(request, 
+    'listings/band_list.html', 
+    {'bands': bands})
+
+def band_detail(request, band_id):
+    band = Band.objects.get(id=band_id)
+    return render(request,
+            'listings/band_detail.html',
+            {'band': band})
 
 def about(request):
-    return HttpResponse('<h1>A propos</h1><p>Nous aimons merch</p>')
+    return render(request, 'listings/about.html')
 
-def listings(request):
+def listing_list(request):
     listings = Listing.objects.all()
-    return HttpResponse(f"""
-        <h1>Liste des titres!</h1>
-        <p>Ma liste:</p>
-        <ul>
-            <li>{listings[0].title}</li>
-            <li>{listings[1].title}</li>
-            <li>{listings[2].title}</li>
-            <li>{listings[3].title}</li>
-            <li>{listings[4].title}</li>
-            <li>{listings[5].title}</li>
-            <li>{listings[6].title}</li>
-        </ul>
-    """)
+    return render(request, 'listings/listing_list.html', {'listings': listings})
 
+def listing_detail(request, listing_id):
+    listing = Listing.objects.get(id=listing_id)
+    return render(request, 
+                    'listings/listing_detail.html',
+                    {'listing': listing})
+
+def listing_where_band(request, band_id):
+    band = Band.objects.get(id=band_id)
+    return render(request,
+                    'listings/listing_where_band.html',
+                    {'band': band})
 def contact(request):
-    return HttpResponse('<h1>Nous contacter</h1><p>Page de contact<p>')
+    return render(request, 'listings/contact.html')
