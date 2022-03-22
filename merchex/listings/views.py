17261425636dc16689b94ae2f51200f4f0d02166
Eddy-123 <adegnandjoueddy12@gmail.com>
2022-03-22 11:29:29 +0100

    CRUD completed

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -31,6 +31,31 @@ def band_create(request):
                     'listings/band_create.html',
                     {'form': form})
 
+def band_update(request, band_id):
+    band = Band.objects.get(id=band_id)
+
+    if request.method == 'POST':
+        form = BandForm(request.POST, instance=band)
+        if form.is_valid():
+            form.save()
+            return redirect('band-detail', band.id)
+    else:
+        form = BandForm(instance=band)
+    
+    return render(request,
+                    'listings/band_update.html',
+                    {'form': form})
+
+def band_delete(request, band_id):
+    band = Band.objects.get(id=band_id)
+    if request.method == 'POST':
+        band.delete()
+        return redirect('band-list')
+    
+    return render(request, 
+                    'listings/band_delete.html',
+                    {'band': band})
+
 def about(request):
     return render(request, 'listings/about.html')
 
@@ -61,6 +86,28 @@ def listing_create(request):
     return render(request, 
                     'listings/listing_create.html',
                     {'form': form})
+
+def listing_update(request, listing_id):
+    listing = Listing.objects.get(id=listing_id)
+    if request.method == 'POST':
+        form = ListingForm(request.POST, instance=listing)
+        if form.is_valid():
+            form.save()
+            return redirect('listing-detail', listing.id)
+    else:
+        form = ListingForm(instance=listing)
+    return render(request, 
+                    'listings/listing_update.html',
+                    {'form': form})
+
+def listing_delete(request, listing_id):
+    listing = Listing.objects.get(id=listing_id)
+    if request.method == 'POST':
+        listing.delete()
+        return redirect('listing-list')
+    return render(request, 
+                    'listings/listing_delete.html',
+                    {'listing': listing})
 
 def contact(request):
     if request.method == 'POST':
